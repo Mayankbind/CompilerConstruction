ab24687387140e2a6527dbeb203fb77d349dcf65
Mayank Bind <mayankbind@Mayanks-MacBook-Air.local>
2024-12-23 21:19:44 +0530

    first_commit

---
+++ practical1.c
@@ -0,0 +1,20 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    char str[50];
+    printf("Enter the string: ");
+    scanf("%s",str);
+    int a_count = 0,i=0;
+    while(str[i]=='a'){
+        i++;
+        a_count++;
+    }
+    if(str[i]=='b' && str[i+1]=='b' && str[i+2]=='\0'){
+        printf("Valid String");
+    }else{
+        printf("Invalid Stirng");
+    }
+    return 0;
+}
